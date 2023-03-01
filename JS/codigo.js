9f6d337777389df5c9818f06bf0b4c00c4117a38
Pablo <111131311+profeInformatica101@users.noreply.github.com>
2023-03-01 13:53:49 +0100

    Add files via upload

---
+++ JS/codigo.js
@@ -0,0 +1,29 @@
+let datum;
+function readData(file,id){
+    console.log("Leyendo los datos");
+    d3.csv(file, processData)
+    .then((data) => graph(data,id))
+    .catch((error)=> console.log("error: "+error.message));
+}
+
+function processData(datum){
+    return datum;
+}
+
+function graph(data, id){
+    datum = data;
+    console.log(datum);
+    //console.log(id,data);
+    configuracion();
+    principal();
+}
+
+
+window.onload = function () {
+    readData(
+      "https://raw.githubusercontent.com/profeInformatica101/HojasDeCalculo/main/titanic3.csv",
+      "#contenedor"
+    );
+  };
+
+  //v2 --> https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv
